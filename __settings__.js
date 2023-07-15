cd139d35e63c6dfe7639048eb4197c6ab11be9f6
Willy Sun <sunfanyun@Willys-MacBook-Pro.local>
2023-07-15 20:29:38 +0100

    init project

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1783394.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 140116774, 140116775, 140116776, 140116777 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/140116798/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/140116799/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/140116797/1/ammo.js', 'preload' : true},
+];
