bc9b7ca13ba949a052f3b31fdfba943e001374bd
Willy Sun <sunfanyun@pc-136-114.customer.ask4.lan>
2023-07-17 11:02:59 +0100

    update __settings__.js with absolute path

--- __settings__.js
+++ __settings__.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "1783394.json";
+window.SCENE_PATH = "https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/1783394.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
@@ -9,7 +9,7 @@ window.CONTEXT_OPTIONS = {
     'powerPreference': "default"
 };
 window.SCRIPTS = [ 140116774, 140116775, 140116776, 140116777 ];
-window.CONFIG_FILENAME = "config.json";
+window.CONFIG_FILENAME = "https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
@@ -18,5 +18,8 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
-    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/140116798/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/140116799/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/140116797/1/ammo.js', 'preload' : true},
+  {'moduleName' : 'Ammo',
+    'glueUrl' : 'https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/files/assets/140116798/1/ammo.wasm.js',
+    'wasmUrl' : 'https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/files/assets/140116799/1/ammo.wasm.wasm',
+    'fallbackUrl' : 'https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/files/assets/140116797/1/ammo.js', 'preload' : true},
 ];
