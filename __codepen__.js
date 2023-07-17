5f6dba21bea7bf307146d9de66b34882ed579efd
Willy Sun <sunfanyun@pc-136-114.customer.ask4.lan>
2023-07-17 14:14:12 +0100

    use amazon s3 paths

--- __codepen__.js
+++ __codepen__.js
@@ -1,3 +1,48 @@
+var loadModules = function (modules, urlPrefix, doneCallback) { // eslint-disable-line no-unused-vars
+
+    if (typeof modules === "undefined" || modules.length === 0) {
+        // caller may depend on callback behaviour being async
+        setTimeout(doneCallback);
+    } else {
+        let remaining = modules.length;
+        const moduleLoaded = () => {
+            if (--remaining === 0) {
+                doneCallback();
+            }
+        };
+
+        modules.forEach(function (m) {
+            pc.WasmModule.setConfig(m.moduleName, {
+                glueUrl: urlPrefix + m.glueUrl,
+                wasmUrl: urlPrefix + m.wasmUrl,
+                fallbackUrl: urlPrefix + m.fallbackUrl
+            });
+
+            if (!m.hasOwnProperty('preload') || m.preload) {
+                if (m.moduleName === 'BASIS') {
+                    // preload basis transcoder
+                    pc.basisInitialize();
+                    moduleLoaded();
+                } else if (m.moduleName === 'DracoDecoderModule') {
+                    // preload draco decoder
+                    if (pc.dracoInitialize) {
+                        // 1.63 onwards
+                        pc.dracoInitialize();
+                        moduleLoaded();
+                    } else {
+                        // 1.62 and earlier
+                        pc.WasmModule.getInstance(m.moduleName, () => { moduleLoaded(); });
+                    }
+                } else {
+                    // load remaining modules in global scope
+                    pc.WasmModule.getInstance(m.moduleName, () => { moduleLoaded(); });
+                }
+            } else {
+                moduleLoaded();
+            }
+        });
+    }
+};
 (function () {
     // Shared Lib
     var CANVAS_ID = 'application-canvas';
@@ -215,6 +260,8 @@
 
                       app.start();
 
+                      //var ball = app.root.findByName('ball');
+                      //ball.destroy();
                       //console.log(app.root.children);
                       //console.log(app.root.children[0].children);
                       //for (var i = 0; i < 10; i++) {
@@ -233,3 +280,115 @@
         configure();
     }
 })();
+pc.script.createLoadingScreen(function (app) {
+    var showSplash = function () {
+        // splash wrapper
+        var wrapper = document.createElement('div');
+        wrapper.id = 'application-splash-wrapper';
+        document.body.appendChild(wrapper);
+
+        // splash
+        var splash = document.createElement('div');
+        splash.id = 'application-splash';
+        wrapper.appendChild(splash);
+        splash.style.display = 'none';
+
+        var logo = document.createElement('img');
+        logo.src = ASSET_PREFIX + 'logo.png';
+        splash.appendChild(logo);
+        logo.onload = function () {
+            splash.style.display = 'block';
+        };
+
+        var container = document.createElement('div');
+        container.id = 'progress-bar-container';
+        splash.appendChild(container);
+
+        var bar = document.createElement('div');
+        bar.id = 'progress-bar';
+        container.appendChild(bar);
+
+    };
+
+    var hideSplash = function () {
+        var splash = document.getElementById('application-splash-wrapper');
+        splash.parentElement.removeChild(splash);
+    };
+
+    var setProgress = function (value) {
+        var bar = document.getElementById('progress-bar');
+        if (bar) {
+            value = Math.min(1, Math.max(0, value));
+            bar.style.width = value * 100 + '%';
+        }
+    };
+
+    var createCss = function () {
+        var css = [
+            'body {',
+            '    background-color: #283538;',
+            '}',
+
+            '#application-splash-wrapper {',
+            '    position: absolute;',
+            '    top: 0;',
+            '    left: 0;',
+            '    height: 100%;',
+            '    width: 100%;',
+            '    background-color: #283538;',
+            '}',
+
+            '#application-splash {',
+            '    position: absolute;',
+            '    top: calc(50% - 28px);',
+            '    width: 264px;',
+            '    left: calc(50% - 132px);',
+            '}',
+
+            '#application-splash img {',
+            '    width: 100%;',
+            '}',
+
+            '#progress-bar-container {',
+            '    margin: 20px auto 0 auto;',
+            '    height: 2px;',
+            '    width: 100%;',
+            '    background-color: #1d292c;',
+            '}',
+
+            '#progress-bar {',
+            '    width: 0%;',
+            '    height: 100%;',
+            '    background-color: #f60;',
+            '}',
+            '@media (max-width: 480px) {',
+            '    #application-splash {',
+            '        width: 170px;',
+            '        left: calc(50% - 85px);',
+            '    }',
+            '}'
+
+        ].join('\n');
+
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+
+        document.head.appendChild(style);
+    };
+
+
+    createCss();
+
+    showSplash();
+
+    app.on('preload:end', function () {
+        app.off('preload:progress');
+    });
+    app.on('preload:progress', setProgress);
+    app.on('start', hideSplash);
+});
--- __settings__.js
+++ __settings__.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/1783394.json";
+window.SCENE_PATH = "https://playcanvas-public.s3.amazonaws.com/test-playcanvas/1783394.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
@@ -9,7 +9,7 @@ window.CONTEXT_OPTIONS = {
     'powerPreference': "default"
 };
 window.SCRIPTS = [ 140116774, 140116775, 140116776, 140116777 ];
-window.CONFIG_FILENAME = "https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/config.json";
+window.CONFIG_FILENAME = "https://playcanvas-public.s3.amazonaws.com/test-playcanvas/config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
@@ -18,4 +18,9 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
+  {'moduleName' : 'Ammo',
+    'glueUrl' : 'https://playcanvas-public.s3.amazonaws.com/test-playcanvas/files/assets/140116798/1/ammo.wasm.js',
+    'wasmUrl' : 'https://playcanvas-public.s3.amazonaws.com/test-playcanvas/files/assets/140116799/1/ammo.wasm.wasm',
+    'fallbackUrl' : 'https://playcanvas-public.s3.amazonaws.com/test-playcanvas/files/assets/140116797/1/ammo.js', 'preload' : true},
 ];
+
