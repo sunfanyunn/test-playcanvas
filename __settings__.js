1cc46e31657cd2ecc37d7111131269cc492b8a85
Willy Sun <sunfanyun@pc-136-114.customer.ask4.lan>
2023-07-17 11:17:16 +0100

    remove preload modules

--- __settings__.js
+++ __settings__.js
@@ -18,8 +18,4 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
-  {'moduleName' : 'Ammo',
-    'glueUrl' : 'https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/files/assets/140116798/1/ammo.wasm.js',
-    'wasmUrl' : 'https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/files/assets/140116799/1/ammo.wasm.wasm',
-    'fallbackUrl' : 'https://raw.githubusercontent.com/sunfanyunn/test-playcanvas/main/files/assets/140116797/1/ammo.js', 'preload' : true},
 ];
